08cb1d983972b782b161b7391c07733bf6deaee9
arshWebDev <arshgoyalDev@gmail.com>
2022-04-09 21:24:13 +0530

    installed & set-up react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Url Shortener</h1>
+      <Routes>
+        <Route path="/" element={<h1>Url Shortener</h1>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import "./index.css";
 
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <>
+    <BrowserRouter>
+      <App tab="home" />
+    </BrowserRouter>
+  </>
+);

cc30f4c1bbeab5ce778bfe5621e2b1798f2bcd35
arshWebDev <arshgoyalDev@gmail.com>
2022-04-09 21:17:17 +0530

    installed & set-up firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoShhkhpkKDq9Ce33rtK1etdzV6mFkLSI",
+  authDomain: "url-shortener-7b83e.firebaseapp.com",
+  projectId: "url-shortener-7b83e",
+  storageBucket: "url-shortener-7b83e.appspot.com",
+  messagingSenderId: "953393313541",
+  appId: "1:953393313541:web:95675f0b2a25fecda9296c"
+};
+
+const app = initializeApp(firebaseConfig);

bbc847bcc06989fd73a3044db701f0119e4ec5a2
arshWebDev <arshgoyalDev@gmail.com>
2022-04-10 22:15:05 +0530

    made header responsive

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -6,12 +6,12 @@ const Header = ({ activeLink }) => {
   const [menuVisible, setMenuVisible] = useState(false);
 
   return (
-    <header className="relative flex justify-between items-center py-6 px-5">
+    <header className="relative flex justify-between lg:justify-start items-center lg:gap-8 py-6 lg:py-8 px-5 lg:px-20 xl:px-36">
       <img src={logo} alt="Shortly" />
 
       <button
         aria-label="menu-toggle-btn"
-        className="p-1 flex flex-col gap-[5.5px] w-8 h-7"
+        className="p-1 flex lg:hidden flex-col gap-[5.5px] w-8 h-7"
         onClick={() => {
           setMenuVisible(!menuVisible);
         }}
@@ -22,7 +22,7 @@ const Header = ({ activeLink }) => {
       </button>
 
       <nav
-        className={`absolute top-20 right-5 left-5 flex flex-col items-center py-6 px-5 bg-[#3a3053] text-white font-bold rounded-lg transition-all
+        className={`absolute lg:static top-20 right-5 left-5 sm:left-auto sm:w-[360px] lg:w-full flex flex-col lg:flex-row lg:justify-between items-center py-6 px-5 lg:p-0 bg-[#3a3053] lg:bg-transparent text-white lg:text-black font-bold rounded-xl transition-all lg:scale-100 lg:translate-x-0 lg:translate-y-0
       ${
         menuVisible
           ? "scale-100 translate-x-0 translate-y-0"
@@ -30,15 +30,15 @@ const Header = ({ activeLink }) => {
       }
       `}
       >
-        <ul className="flex flex-col items-center">
-          <li className="py-3 px-4 rounded-md">Features</li>
-          <li className="py-3 px-4 rounded-md">Pricing</li>
-          <li className="py-3 px-4 rounded-md">Resources</li>
+        <ul className="flex flex-col lg:flex-row items-center lg:text-gray-400">
+          <li className="py-3 px-4 rounded-md hover:text-black transition-colors">Features</li>
+          <li className="py-3 px-4 rounded-md hover:text-black transition-colors">Pricing</li>
+          <li className="py-3 px-4 rounded-md hover:text-black transition-colors">Resources</li>
         </ul>
 
-        <div className="flex flex-col items-center gap-3 w-full mt-4 pt-3 border-t border-solid border-[#8f78c9]">
-          <button className="w-full py-2.5 font-bold">Login</button>
-          <button className="w-full py-2.5 font-bold bg-[#2bd1cf] rounded-full">
+        <div className="flex flex-col lg:flex-row items-center gap-3 w-full lg:w-auto mt-4 lg:mt-0 pt-3 lg:pt-0 border-t border-solid lg:border-none border-[#8f78c9]">
+          <button className="py-2.5 px-8 font-bold lg:text-gray-400">Login</button>
+          <button className="py-2.5 px-8 text-white font-bold bg-[#2bd1cf] rounded-full">
             Sign Up
           </button>
         </div>

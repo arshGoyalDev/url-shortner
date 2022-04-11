1f9131a7f9f7600361b46c17e26892089822e838
arshWebDev <arshgoyalDev@gmail.com>
2022-04-11 23:34:53 +0530

    add: sign-up with facebook in sign-up page

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -65,14 +65,23 @@ const SignUp = () => {
           </button>
         </form>
         <span className="text-gray-300 text-center w-full block my-4">OR</span>
-        <button className="flex items-center justify-center gap-4 py-3 px-8 font-medium w-full rounded-xl border-2 border-solid border-gray-300 text-gray-500 transition-all">
-          <div className="w-5">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-              <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-            </svg>
-          </div>
-          Sign Up with Google
-        </button>
+        <div className="flex items-center gap-2">
+          <button className="grid place-items-center py-3 px-8 font-medium w-full rounded-xl border-2 border-solid border-gray-300 text-gray-500 transition-all">
+            <div className="w-5">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+              </svg>
+            </div>
+          </button>
+
+          <button className="grid place-items-center py-3 px-8 font-medium w-full rounded-xl border-2 border-solid border-gray-300 text-gray-500 transition-all">
+            <div className="w-3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+              </svg>
+            </div>
+          </button>
+        </div>
 
         <p className="text-gray-500 text-center mt-7 text-sm">
           Already have an account?{" "}

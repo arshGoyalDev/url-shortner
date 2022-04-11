adb3b4e14ad7b6d67cb8bcb7eb5fcb84a73137b2
arshWebDev <arshgoyalDev@gmail.com>
2022-04-11 23:05:56 +0530

    created & styled sign-up page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
+import SignUp from "./pages/sign-up";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </div>

993b5513feb6986cb4505fec9b55fdd468f75d30
arshWebDev <arshgoyalDev@gmail.com>
2022-04-12 23:33:59 +0530

    fix: tailwind class issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -67,15 +67,15 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center sm:p-10">
+    <div className="flex items-center justify-center w-full min-h-screen sm:p-10">
       <Helmet>
         <title>Login - Shortly</title>
       </Helmet>
 
-      <div className="py-10 sm:py-12 px-5 sm:px-12 w-[90%] max-w-[440px] sm:shadow-2xl sm:bg-white sm:shadow-gray-200 sm:rounded-2xl">
+      <div className="w-[90%] max-w-[440px] py-10 sm:py-12 px-5 sm:px-12 sm:bg-white sm:shadow-2xl sm:shadow-gray-200 sm:rounded-2xl">
         <h2 className="text-3xl font-semibold">Login</h2>
 
-        <button className="flex justify-center items-center gap-2 py-3 px-8 mt-7 font-medium w-full rounded-xl border-2 border-solid border-gray-300 text-gray-500 transition-all">
+        <button className="flex justify-center items-center gap-2 w-full text-gray-500 font-medium py-3 px-8 border-2 border-solid border-gray-300 mt-7 rounded-xl transition-all">
           <div className="w-5">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
               <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
@@ -84,7 +84,7 @@ const Login = () => {
           <span className="text-sm">Login with Google</span>
         </button>
 
-        <span className="my-6 block text-center text-gray-400">
+        <span className="block text-center text-gray-400 my-6">
           Or, login with
         </span>
 
@@ -104,7 +104,7 @@ const Login = () => {
                 placeholder="email address"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className={`bg-gray-100 rounded-xl py-3 px-4 text-sm w-full border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none ${
+                className={`w-full text-sm py-3 px-4 bg-gray-100 border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none rounded-xl ${
                   error.email ? "border-red-600" : "border-gray-100"
                 }`}
               />
@@ -128,7 +128,7 @@ const Login = () => {
                 placeholder="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className={`bg-gray-100 rounded-xl py-3 px-4 text-sm w-full border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none ${
+                className={`w-full text-sm py-3 px-4 bg-gray-100 border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none rounded-xl ${
                   error.password ? "border-red-600" : "border-gray-100"
                 }`}
               />
@@ -140,7 +140,7 @@ const Login = () => {
             </div>
           </div>
 
-          <button className="mt-5 py-3 px-8 text-white font-medium bg-[#2bd1cf] w-full lg:hover:bg-opacity-60 rounded-xl transition-all">
+          <button className="w-full text-white font-medium py-3 px-8 bg-[#2bd1cf] lg:hover:bg-opacity-60 mt-5 rounded-xl transition-all">
             Login
           </button>
         </form>
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -68,15 +68,15 @@ const SignUp = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center sm:p-10">
+    <div className="flex items-center justify-center w-full min-h-screen sm:p-10">
       <Helmet>
         <title>Sign Up - Shortly</title>
       </Helmet>
 
-      <div className="py-10 sm:py-12 px-5 sm:px-12 w-[90%] max-w-[440px] sm:shadow-2xl sm:bg-white sm:shadow-gray-200 sm:rounded-2xl">
+      <div className="w-[90%] max-w-[440px] py-10 sm:py-12 px-5 sm:px-12 sm:bg-white rounded-2xl sm:shadow-2xl sm:shadow-gray-200">
         <h2 className="text-3xl font-semibold">Sign Up</h2>
 
-          <button className="flex justify-center items-center gap-3 py-3 px-8 mt-7 font-medium w-full rounded-xl border-2 border-solid border-gray-300 text-gray-500 transition-all">
+          <button className="flex justify-center items-center gap-3 w-full font-medium text-gray-500 py-3 px-8 border-2 border-solid border-gray-300 mt-7 rounded-xl transition-all">
             <div className="w-5">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                 <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
@@ -86,7 +86,7 @@ const SignUp = () => {
           </button>
 
 
-        <span className="my-6 block text-center text-gray-400">
+        <span className="block text-center text-gray-400 my-6">
           Or, sign up with
         </span>
 
@@ -106,7 +106,7 @@ const SignUp = () => {
                 placeholder="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className={`bg-gray-100 rounded-xl py-3 px-4 text-sm w-full border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none ${
+                className={`w-full text-sm py-3 px-4 bg-gray-100 border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none rounded-xl ${
                   error.username ? "border-red-600" : "border-gray-100"
                 }`}
               />
@@ -130,7 +130,7 @@ const SignUp = () => {
                 placeholder="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className={`bg-gray-100 rounded-xl py-3 px-4 text-sm w-full border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none ${
+                className={`w-full text-sm py-3 px-4 bg-gray-100 border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none rounded-xl ${
                   error.email ? "border-red-600" : "border-gray-100"
                 }`}
               />
@@ -154,7 +154,7 @@ const SignUp = () => {
                 placeholder="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className={`bg-gray-100 rounded-xl py-3 px-4 text-sm w-full border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none ${
+                className={`w-full text-sm py-3 px-4 bg-gray-100 border-2 border-solid border-gray-100 focus:border-[#2bd1cf] focus:outline-none rounded-xl ${
                   error.password ? "border-red-600" : "border-gray-100"
                 }`}
               />
@@ -166,12 +166,12 @@ const SignUp = () => {
             </div>
           </div>
 
-          <button className="mt-5 py-3 px-8 text-white font-medium bg-[#2bd1cf] w-full lg:hover:bg-opacity-60 rounded-xl transition-all">
+          <button className="w-full text-white font-medium py-3 px-8 bg-[#2bd1cf] lg:hover:bg-opacity-60 mt-5 rounded-xl transition-all">
             Create Account
           </button>
         </form>
 
-        <p className="text-gray-500 text-center mt-4 text-sm">
+        <p className="text-gray-500 text-center text-sm mt-4">
           Already have an account?{" "}
           <Link to="/login" className="text-black font-semibold">
             Login

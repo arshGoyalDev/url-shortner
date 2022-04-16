aa9046a4554e48914df2c890200d142e6621e7ee
arshWebDev <arshgoyalDev@gmail.com>
2022-04-16 16:56:25 +0530

    add: color palette in tailwind config

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -19,13 +19,13 @@ const Header = ({ activeLink }) => {
           setMenuVisible(!menuVisible);
         }}
       >
-        <span className="block w-full h-[3px] bg-gray-400 rounded"></span>
-        <span className="block w-full h-[3px] bg-gray-400 rounded"></span>
-        <span className="block w-full h-[3px] bg-gray-400 rounded"></span>
+        <span className="block w-full h-[3px] bg-neutral-gray rounded"></span>
+        <span className="block w-full h-[3px] bg-neutral-gray rounded"></span>
+        <span className="block w-full h-[3px] bg-neutral-gray rounded"></span>
       </button>
 
       <nav
-        className={`absolute lg:static top-20 right-5 left-5 sm:left-auto flex flex-col lg:flex-row lg:justify-between items-center sm:w-[360px] lg:w-full text-white lg:text-black font-medium py-6 px-5 lg:p-0 bg-[#3a3053] lg:bg-transparent rounded-xl lg:scale-100 lg:translate-x-0 lg:translate-y-0 transition-all
+        className={`absolute lg:static top-20 right-5 left-5 sm:left-auto flex flex-col lg:flex-row lg:justify-between items-center sm:w-[360px] lg:w-full text-white lg:text-black font-medium py-6 px-5 lg:p-0 bg-primary-darkVoilet lg:bg-transparent rounded-xl lg:scale-100 lg:translate-x-0 lg:translate-y-0 transition-all
       ${
         menuVisible
           ? "scale-100 translate-x-0 translate-y-0"
@@ -33,7 +33,7 @@ const Header = ({ activeLink }) => {
       }
       `}
       >
-        <ul className="flex flex-col lg:flex-row items-center lg:text-gray-400">
+        <ul className="flex flex-col lg:flex-row items-center lg:text-neutral-grayishViolet lg:font-semibold">
           <Link to="/features">
             <li className="py-3 px-4 rounded-md hover:text-black transition-colors">
               Features
@@ -51,14 +51,14 @@ const Header = ({ activeLink }) => {
           </Link>
         </ul>
 
-        <div className="flex flex-col lg:flex-row items-center gap-3 w-full lg:w-auto pt-3 lg:pt-0 border-t border-solid lg:border-none border-[#8f78c9] mt-4 lg:mt-0">
+        <div className="flex flex-col lg:flex-row items-center gap-3 w-full lg:w-auto pt-7 pb-3 lg:py-0 border-t border-solid lg:border-none border-[#8f78c9] mt-4 lg:mt-0">
           <Link to="/login">
-            <button className="font-medium lg:text-gray-400 lg:hover:text-black py-2.5 px-8 transition-all">
+            <button className="font-semibold lg:text-neutral-grayishViolet lg:hover:text-black lg:py-2.5 lg:px-8 transition-all">
               Login
             </button>
           </Link>
           <Link to="/sign-up">
-            <button className="text-white font-semibold py-2.5 px-8 bg-[#2bd1cf] lg:hover:bg-opacity-50 rounded-full transition-all">
+            <button className="text-white font-semibold py-2.5 px-8 bg-primary-cyan lg:hover:bg-opacity-50 rounded-full transition-all">
               Sign Up
             </button>
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          darkVoilet: "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          grayishViolet: "hsl(257, 7%, 63%)",
+          darkBlue: "hsl(255, 11%, 22%)",
+          darkViolet: "hsl(260, 8%, 14%)",
+        },
+      },
+    },
   },
+
   plugins: [],
-}
+};

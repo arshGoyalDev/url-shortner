f5c45a2d69192fc5a78d0fa68d24b568fe827c5e
--login <arshgoyalDev@gmail.com>
2022-04-23 22:20:30 +0530

    add: fetching link on url form submit

--- src/components/UrlShortener.js
+++ src/components/UrlShortener.js
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 const UrlShortener = () => {
-  const [link, setLink] = useState("");
+  const [url, setUrl] = useState("");
+  const [shortenedLinks, setShortenedLinks] = useState([]);
   const [error, setError] = useState(false);
 
-  const shortenLink = (e) => {
+  const shortenLink = async (e) => {
     e.preventDefault();
 
-    if (link.replaceAll(' ', '') === "") {
+    if (url.replaceAll(" ", "") === "") {
       setError(true);
-      return
+      return;
     }
+
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    const fetchedLink = await res.json();
+
+    fetchedLink.ok
+      ? setShortenedLinks([...shortenedLinks, fetchedLink.result])
+      : setError(true);
   };
+
+  console.log(shortenedLinks);
 
   return (
     <form onSubmit={shortenLink}>
@@ -19,21 +29,29 @@ const UrlShortener = () => {
         <div className="w-full relative">
           <input
             type="text"
-            name="link"
-            id="link"
-            value={link}
-            onFocus={() => setError(false)}
-            onChange={(e) => setLink(e.target.value)}
-            className={`w-full py-3 md:py-4 px-4 md:px-7 border-4 border-solid focus:border-primary-cyan rounded-md focus:outline-none transition-all duration-300 ${error ? "border-secondary-red" : "border-transparent"}`}
+            name="url"
+            id="url"
+            value={url}
+            // onFocus={() => setError(false)}
+            onChange={(e) => setUrl(e.target.value)}
+            className={`w-full py-3 md:py-4 px-4 md:px-7 border-4 border-solid focus:border-primary-cyan rounded-md focus:outline-none transition-all duration-300 ${
+              error ? "border-secondary-red" : "border-transparent"
+            }`}
           />
           <span
             className={`placeholder-text absolute top-1/2 left-4 md:left-7 -translate-y-1/2 text-neutral-grayishViolet font-medium text-sm transition-all duration-300 pointer-events-none ${
-              link !== "" && "left-10 opacity-0 invisible"
+              url !== "" && "left-10 opacity-0 invisible"
             } ${error && "text-secondary-red"}`}
           >
-            Shorten a link here...
+            Shorten a url here...
           </span>
-          <span className={`absolute -bottom-6 z-30 left-2 w-max text-secondary-red text-sm transition-all duration-300 ${error ? "visible opacity-100" : "invisible opacity-0"}`}>Please add a link</span>
+          <span
+            className={`absolute -bottom-6 z-30 left-2 w-max text-secondary-red text-sm transition-all duration-300 ${
+              error ? "visible opacity-100" : "invisible opacity-0"
+            }`}
+          >
+            Please add a url
+          </span>
         </div>
         <button className="w-full md:w-[180px] text-white font-semibold py-3 md:py-4 px-4 md:px-8 bg-primary-cyan hover:bg-[#63dddd] rounded-md transition-colors duration-200">
           Shorten it!

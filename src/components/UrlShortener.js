d4458ffb39051f7736deabb5049debc3a17d4d6a
--login <arshgoyalDev@gmail.com>
2022-04-23 21:58:11 +0530

    add: error state in the link input

--- src/components/UrlShortener.js
+++ src/components/UrlShortener.js
@@ -2,30 +2,38 @@ import { useState } from "react";
 
 const UrlShortener = () => {
   const [link, setLink] = useState("");
+  const [error, setError] = useState(false);
 
   const shortenLink = (e) => {
     e.preventDefault();
+
+    if (link.replaceAll(' ', '') === "") {
+      setError(true);
+      return
+    }
   };
 
   return (
     <form onSubmit={shortenLink}>
-      <div className="url-shortener flex flex-col md:flex-row gap-5 py-8 md:py-10 px-6 md:px-12 mx-5 lg:mx-20 xl:mx-36 -mt-20 md:-mt-16 rounded-lg">
+      <div className="url-shortener flex flex-col md:flex-row gap-7 py-8 md:py-10 px-6 md:px-12 mx-5 lg:mx-20 xl:mx-36 -mt-20 md:-mt-16 rounded-lg">
         <div className="w-full relative">
           <input
             type="text"
             name="link"
             id="link"
             value={link}
+            onFocus={() => setError(false)}
             onChange={(e) => setLink(e.target.value)}
-            className="w-full py-3 md:py-4 px-4 md:px-7 rounded-md focus:outline-none"
+            className={`w-full py-3 md:py-4 px-4 md:px-7 border-4 border-solid focus:border-primary-cyan rounded-md focus:outline-none transition-all duration-300 ${error ? "border-secondary-red" : "border-transparent"}`}
           />
           <span
-            className={`absolute top-1/2 left-4 md:left-7 -translate-y-1/2 text-neutral-grayishViolet font-medium text-sm transition-all duration-300 pointer-events-none ${
+            className={`placeholder-text absolute top-1/2 left-4 md:left-7 -translate-y-1/2 text-neutral-grayishViolet font-medium text-sm transition-all duration-300 pointer-events-none ${
               link !== "" && "left-10 opacity-0 invisible"
-            }`}
+            } ${error && "text-secondary-red"}`}
           >
             Shorten a link here...
           </span>
+          <span className={`absolute -bottom-6 z-30 left-2 w-max text-secondary-red text-sm transition-all duration-300 ${error ? "visible opacity-100" : "invisible opacity-0"}`}>Please add a link</span>
         </div>
         <button className="w-full md:w-[180px] text-white font-semibold py-3 md:py-4 px-4 md:px-8 bg-primary-cyan hover:bg-[#63dddd] rounded-md transition-colors duration-200">
           Shorten it!

73e44785b0e3b3c0ea4a2cfe8a11045ee080a913
--login <arshgoyalDev@gmail.com>
2022-04-24 23:09:06 +0530

    add: styled call to action component

--- src/components/sections/CallToAction.js
+++ src/components/sections/CallToAction.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
-  )
-}
+    <section className="call-to-action flex flex-col gap-6 lg:gap-8 items-center justify-center py-24 mt-20 md:mt-28 lg:mt-36">
+      <h2 className="text-3xl lg:text-4xl font-bold text-white">Boost your links today</h2>
+      <button className="text-white lg:text-lg font-semibold py-2.5 lg:py-4 px-8 lg:px-10 bg-primary-cyan lg:hover:bg-[#58fafa] rounded-full transition-colors">
+        Get Started
+      </button>
+    </section>
+  );
+};
 
-export default CallToAction
+export default CallToAction;

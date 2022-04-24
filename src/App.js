dc80ced5fab6ae666f396d533f98a6852828407b
--login <arshgoyalDev@gmail.com>
2022-04-24 22:35:19 +0530

    add: call-to-action component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Header, Hero, Features } from "./components/sections";
+import { Header, Hero, Features, CallToAction } from "./components/sections";
 import UrlShortener from "./components/UrlShortener";
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
 
       <UrlShortener />
       <Features />
+      <CallToAction />
     </main>
   );
 };

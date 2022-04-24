72308f4b8eb6d0a04236fb3d93a688e4185376ea
--login <arshgoyalDev@gmail.com>
2022-04-24 22:37:34 +0530

    fix: a styling issue in features section

--- src/components/sections/Features.js
+++ src/components/sections/Features.js
@@ -29,11 +29,11 @@ const featuresArray = [
 const Features = () => {
   return (
     <div className="px-5 lg:px-20 xl:px-36 mt-20">
-      <div className="flex flex-col gap-6 text-center">
+      <div className="flex flex-col items-center gap-6 text-center">
         <h2 className="text-3xl font-bold text-neutral-darkViolet">
           Advanced Statistics
         </h2>
-        <p className="font-medium text-neutral-grayishViolet">
+        <p className="max-w-[480px] font-medium text-neutral-grayishViolet">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
